c13078eb61a7076d93c3bff337d4f095afcb6403
Danilo Gimenez <dnlogmnz@gmail.com>
2026-02-24 10:53:03 -0300

    fix: Remover validação de PATH para Git for Windows
    Os scipts validavam se \<diretório-de-instalação-do-git\>/cmd estava no PATH.
    * Essa validação não faz sentido, porque o Git for Windows faz isso automaticamente.
    * Ao iniciar um novo Git Bash, o <diretório-de-instalação-do-git> é montado em / (raiz).
    * E, logo em seguida, adiciona o /cmd e o /bin ao PATH.
    * O fix foi remover a validação existente no 11-git-envs.sh.
    Esse script ainda ajusta o ambiente para armazenar o gitconfig em diretório compatível com XDG / FHS.

--- src/home/.config/bash/00-bash-aliases.sh
+++ src/home/.config/bash/00-bash-aliases.sh
@@ -3,10 +3,10 @@
 # Aliases para facilitar o uso do Bash
 # ==========================================================================================
 
-alias ll="ls -l"
-alias la="ls -lA"
-alias grep="grep --color=auto"
-alias npp='start /d/$USERNAME/Apps/Notepad++/notepad++.exe "$@"'
+alias ll="/usr/bin/ls -l --color=auto --show-control-chars"
+alias la="/usr/bin/ls -lA --color=auto --show-control-chars"
+alias grep="/usr/bin/grep --color=auto"
+alias npp='/usr/bin/start /d/$USERNAME/Apps/Notepad++/notepad++.exe "$@"'
 
 #-------------------------------------------------------------------------------------------
 #--- Final do script ~/.config/bash-aliases.sh
